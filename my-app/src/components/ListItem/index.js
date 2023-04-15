6b66b0fc9378349a374056fc169551b9033b76cf
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-04-15 11:26:05 +0100

    Took away check buttons and replaced with css styled checkboxes

--- my-app/src/components/ListItem/index.js
+++ my-app/src/components/ListItem/index.js
@@ -1,21 +1,18 @@
 import React from "react";
 import "./ListItem.css";
 
-function ListItem({item, index, indexDelete,onCheckButton}) {
-
-
+function ListItem({item, index, indexDelete, onCheckButton}) {
   return (
     <div className="list-item">
-      <div className = "list_button_div">
-      <li className="todo-item">{item}</li>
-      <button className="delete_button" onClick={() => indexDelete(index)}> 
-        Delete
-      </button> 
-      <button className="check_button" onClick={onCheckButton}>
-        Check
-      </button>
+      <div className="list_button_div">
+        <li className="todo-item">{item}</li>
+        <button className="delete_button" onClick={() => indexDelete(index)}> 
+          Delete
+        </button> 
+        <input type="checkbox" id={`checkBtn-${index}`} className="checkBtn" onClick={() => onCheckButton(index)}></input>
+        <label htmlFor={`checkBtn-${index}`} className="checkBtn_label"><span>&#10003;</span></label>
       </div>
-    </div> //indexDelete is the function that deletes the item from the list
+    </div>
   );
 }
 
